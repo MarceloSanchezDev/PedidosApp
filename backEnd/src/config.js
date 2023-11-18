4aa24a2b0c7c0ea6be81d84317d2bbd41f04cbde
Marcelo Edgardo Sanchez <marcedgardosanchez@gmail.com>
2023-11-18 19:53:21 -0300

    instale dependencias

--- backEnd/src/config.js
+++ backEnd/src/config.js
@@ -1,8 +1,12 @@
+//llamamos el metodo config de *dotenv*
+require('dotenv').config();
+
 //En config pondre todas las variables globales del sistema
 
 module.exports = {
     //creamos un objeto llamado app
     app: {//creamos un puerto
-        port : process.env.PORT || 4000
+        port : process.env.PORT ,
     }
 }
+
